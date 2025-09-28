0027137b9899bb5369046a9dca0b26764b55c4e5
szabence29@gmail.com <szabence029@gmail.com>
2025-09-28 23:48:13 +0200

    Base auth is done with firebase auth service. It needs a little styling, that will be done in the next commit.

--- CalorieCounter/CalorieCounterApp.swift
+++ CalorieCounter/CalorieCounterApp.swift
@@ -1,8 +1,12 @@
 import SwiftUI
 import SwiftData
+import FirebaseCore
 
 @main
 struct CalorieCounterApp: App {
+    // Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
@@ -18,8 +22,17 @@ struct CalorieCounterApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
         .modelContainer(sharedModelContainer)
+    }
+}
+
+// App delegate for Firebase setup
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
